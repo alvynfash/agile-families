c6de3498975c46b1a9d18e77de5e126244bddbe7
Alvyn Fasuyi <zetagik@gmail.com>
2024-02-14 17:24:23 +0100

    update

--- bin/api/api_handler.dart
+++ bin/api/api_handler.dart
@@ -1,5 +1,6 @@
 import 'package:shelf/shelf.dart';
 
+import 'tools.dart';
 import 'toon_handler.dart';
 
 Future<Response> apiHandler(Request request) async {
@@ -7,5 +8,9 @@ Future<Response> apiHandler(Request request) async {
     return await toonHandler(request);
   }
 
-  return Response.notFound('Route not found.');
+  var response = {
+    'message': 'Agile Family API is live',
+    'api_routes': ['/toons', '/toons/{coming_soon}']
+  };
+  return Tools.createEncodedResponse(response);
 }
