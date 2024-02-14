d07a055aa0bc8b857918a77412efc06419b8877b
Alvyn Fasuyi <zetagik@gmail.com>
2024-02-14 15:45:37 +0100

    Changed file system usage

--- bin/api/root_handler.dart
+++ bin/api/root_handler.dart
@@ -1,12 +1,22 @@
 import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_static/shelf_static.dart';
+import 'package:path/path.dart';
 
 Future<Response> rootHandler(Request request) async {
   while (true) {
+    // Get the URI of the Dart script file
+    var scriptUri = Platform.script;
+
+    // Get the path of the directory containing the Dart script file
+    var scriptPath = Directory.fromUri(scriptUri).path;
+
+    // Construct the path to the directory containing the HTML files
+    var pathToHtmlFiles = dirname(scriptPath);
+
     // Set up a shelf handler for serving static files
     final staticHandler = createStaticHandler(
-      '${Directory.current.path}/bin',
+      pathToHtmlFiles,
       defaultDocument: 'index.html',
     );
 
